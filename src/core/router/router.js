eba66af32cafa0b411c5d31fb7f1aac2e4528ebd
guinitum <artking01kg@gmail.com>
2024-09-16 19:08:56 +0300

    re

--- src/core/router/router.js
+++ src/core/router/router.js
@@ -4,14 +4,35 @@ import { Layout } from '@/components/layout/layout_component';
 
 export class Router {
     layout = null;
-	routes = ROUTES;
-	currentRoute = null;
+    routes = ROUTES;
+    currentRoute = null;
     constructor() {
+        window.addEventListener('popstate', () => {
+            this.handleRouteChange();
+        });
         this.handleRouteChange();
+        this.handleLinks();
     }
+    handleLinks() {
+        document.addEventListener('click', (event) => {
+            const target = event.target.closest('a');
+            if (target) {
+                event.preventDefault();
+                this.navigate(target.href);
+            }
+        });
+    }
+
     getCurrentPath() {
         return window.location.pathname; // Получаем url текущей страницы
     }
+    navigate(path) {
+        if (path !== this.getCurrentPath()) {
+            window.history.pushState({}, '', path);
+            this.handleRouteChange();
+        }
+    }
+
     handleRouteChange() {
         const path = this.getCurrentPath() || '/';
         let route = this.routes.find((route) => route.path === path);
@@ -21,14 +42,11 @@ export class Router {
                 component: NotFound,
             };
         }
-
         this.currentRoute = route;
         this.render();
     }
-
     render() {
         const component = new this.currentRoute.component();
-
         if (!this.layout) {
             this.layout = new Layout({
                 router: this,
