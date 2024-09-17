120946294e37343e230a253bd1974e15dcf2b3fb
guinitum <artking01kg@gmail.com>
2024-09-17 17:17:16 +0300

    20lesson finish

--- src/core/component/base_screen_component.js
+++ src/core/component/base_screen_component.js
@@ -1,9 +1,18 @@
 import { getTitle } from '../../config/seo_config';
 
 export class BaseScreen {
+	/**
+	 * 	Create a new BaseScreen instance
+	 * 	@param {Object} options - The options for the BaseScreen
+	 * @param {string} options.title  - The title for the BaseScreen 
+	 */
     constructor({title}) {
         document.title = getTitle(title);
     }
+	/**
+	 *	Render the child component content
+	 *	@returns {HTMLElement}
+	 */
     render() {
         throw new Error('Render method must be implemented in the child class');
     }
