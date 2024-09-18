4d8f7e0e726fc1616a3af59e2cd9608a64018800
guinitum <artking01kg@gmail.com>
2024-09-18 13:18:19 +0300

    21 lesson start make render service

--- src/core/services/render_service.js
+++ src/core/services/render_service.js
@@ -1,3 +1,5 @@
+import { ChildComponent } from '../component/child_component';
+
 class RenderService {
     /**
      * @param {string} html
@@ -20,16 +22,38 @@ class RenderService {
     replaceComponentTags(parentElement, components) {
         const componentTagPattern = /^component-/; // Регулярное выражение
         const allElements = parentElement.getElementsByTagName('*');
-		
-		for(const element of allElements){
-			const elementTagName  = element.tagName.toLowerCase()
-			if(componentTagPattern.test(elementTagName)){
-				const componentName = elementTagName
-					.replace(componentTagPattern, '')
-					.replace(/-/g, '' )
+    
+        for (const element of allElements) {
+            const elementTagName = element.tagName.toLowerCase();
+            if (componentTagPattern.test(elementTagName)) {
+                const componentName = elementTagName
+                    .replace(componentTagPattern, '')
+                    .replace(/-/g, '');
 
-			}
-		}
+                const foundComponent = components.find((Component) => {
+                    const instance =
+                        Component instanceof ChildComponent
+                            ? Component
+                            : new Component();
+                    return (
+                        instance.constructor.name.toLowerCase() ===
+                        componentName
+                    );
+                });
+
+                if (foundComponent) {
+                    const componentContent =
+                        foundComponent instanceof ChildComponent
+                            ? foundComponent.render()
+                            : new foundComponent().render();
+                    element.replaceWith(componentContent);
+                } else {
+                    console.error(
+                        `Component "${componentName}" not found in the provided components array`,
+                    );
+                }
+            }
+        }
+    }
 }
-
 export default new RenderService();
