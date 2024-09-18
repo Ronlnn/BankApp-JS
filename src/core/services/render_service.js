f897779b22c38f5376bd56d47842d3bb65ff6c4d
guinitum <artking01kg@gmail.com>
2024-09-18 12:23:07 +0300

    21 lesson start make render service

--- src/core/services/render_service.js
+++ src/core/services/render_service.js
@@ -1,3 +1,26 @@
 class RenderService {
-	
+    /**
+     * @param {string} html
+     * @param {Array} components
+     * @param {Object} [styles]
+     * @return {HTMLElement}
+     */
+    htmlToElement(html, components = [], styles) {
+        const template = document.createElement('template');
+        template.innerHTML = html.trim();
+        const element = template.content.firstChild;
+        this.replaceComponentTags(element, components);
+        return element;
+    }
+
+    /**
+     * @param {HTMLElement} parentElement
+     * @param {Array} components
+     */
+    replaceComponentTags(parentElement, components) {
+        const componentTagPattern = /^component-/; // Регулярное выражение
+        const allElements = parentElement.getElementsByTagName('*');
+    }
 }
+
+export default new RenderService();
