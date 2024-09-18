96a65be9d0710c0785f08f04f06a061df2e02076
guinitum <artking01kg@gmail.com>
2024-09-18 13:33:25 +0300

    21 lesson finish

--- src/core/services/render_service.js
+++ src/core/services/render_service.js
@@ -1,59 +1,61 @@
 import { ChildComponent } from '../component/child_component';
 
 class RenderService {
-    /**
-     * @param {string} html
-     * @param {Array} components
-     * @param {Object} [styles]
-     * @return {HTMLElement}
-     */
-    htmlToElement(html, components = [], styles) {
-        const template = document.createElement('template');
-        template.innerHTML = html.trim();
-        const element = template.content.firstChild;
-        this.replaceComponentTags(element, components);
-        return element;
-    }
+	/**
+	 * @param {string} html
+	 * @param {Array} components
+	 * @param {Object} [styles]
+	 * @return {HTMLElement}
+	 */
+	htmlToElement(html, components = [], styles) {
+		const template = document.createElement('template');
+		template.innerHTML = html.trim();
+		const element = template.content.firstChild;
+		this.replaceComponentTags(element, components);
+		return element;
+	}
 
-    /**
-     * @param {HTMLElement} parentElement
-     * @param {Array} components
-     */
-    replaceComponentTags(parentElement, components) {
-        const componentTagPattern = /^component-/; // Регулярное выражение
-        const allElements = parentElement.getElementsByTagName('*');
-    
-        for (const element of allElements) {
-            const elementTagName = element.tagName.toLowerCase();
-            if (componentTagPattern.test(elementTagName)) {
-                const componentName = elementTagName
-                    .replace(componentTagPattern, '')
-                    .replace(/-/g, '');
+	/**
+	 * @param {HTMLElement} parentElement
+	 * @param {Array} components
+	 */
+	replaceComponentTags(parentElement, components) {
+		const componentTagPattern = /^component-/; // Регулярное выражение
+		const allElements = parentElement.getElementsByTagName('*');
 
-                const foundComponent = components.find((Component) => {
-                    const instance =
-                        Component instanceof ChildComponent
-                            ? Component
-                            : new Component();
-                    return (
-                        instance.constructor.name.toLowerCase() ===
-                        componentName
-                    );
-                });
+		for (const element of allElements) {
+			const elementTagName = element.tagName.toLowerCase();
+			if (componentTagPattern.test(elementTagName)) {
+				const componentName = elementTagName
+					.replace(componentTagPattern, '')
+					.replace(/-/g, '');
 
-                if (foundComponent) {
-                    const componentContent =
-                        foundComponent instanceof ChildComponent
-                            ? foundComponent.render()
-                            : new foundComponent().render();
-                    element.replaceWith(componentContent);
-                } else {
-                    console.error(
-                        `Component "${componentName}" not found in the provided components array`,
-                    );
-                }
-            }
-        }
-    }
+				const foundComponent = components.find((Component) => {
+					const instance =
+						Component instanceof ChildComponent
+							? Component
+							: new Component();
+					return (
+						instance.constructor.name.toLowerCase() ===
+						componentName
+					);
+				});
+
+				if (foundComponent) {
+					const componentContent =
+						foundComponent instanceof ChildComponent
+							? foundComponent.render()
+							: new foundComponent().render();
+					element.replaceWith(componentContent);
+				} else {
+					console.error(
+						`Component "${componentName}" not found in the provided components array`,
+					);
+				}
+			}
+		}
+	}
 }
 export default new RenderService();
+
+
